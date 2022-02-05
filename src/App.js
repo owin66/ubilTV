ededd30b53a47205ee5fa4beb1b90567eb32d636
owin66 <78900270+owin66@users.noreply.github.com>
2022-02-05 14:19:48 +0200

    managed component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 
 function App() {
     const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('ТЕКТС В ИНПУТЕ')
 
     function increment() {
         setLikes(likes + 1)
@@ -15,6 +16,11 @@ function App() {
     return (
         <div className="App">
             <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type="text"
+                   value={value}
+                   onChange={event => setValue(event.target.value)}
+            />
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
