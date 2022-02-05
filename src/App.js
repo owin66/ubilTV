394784dfc416f7e528534c06560f6898738a168e
owin66 <78900270+owin66@users.noreply.github.com>
2022-02-05 16:12:31 +0200

    adding new components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('ТЕКТС В ИНПУТЕ')
-
+    const [posts,setPosts] = useState([
+        {id:1,title:'JavaScript',body:'Description'},
+        {id:2,title:'JavaScript 2',body:'Description'},
+        {id:3,title:'JavaScript 3',body:'Description'},
+    ])
+    const [posts2,setPosts2] = useState([
+        {id:1,title:'Python',body:'Description'},
+        {id:2,title:'Python 2',body:'Description'},
+        {id:3,title:'Python 3',body:'Description'},
+    ])
 
     return (
         <div className="App">
-            <ClassCounter />
+        <PostList posts={posts} title='Список постов 1'/>
+            <PostList posts={posts2} title='Список постов 2'/>
         </div>
     );
 }
