49cabb2ff60168ce1b935cd17f73c31a53cabfda
owin66 <78900270+owin66@users.noreply.github.com>
2022-02-05 17:04:03 +0200

    adding props.children in MyButton.jsx

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
-    const [posts,setPosts] = useState([
-        {id:1,title:'JavaScript',body:'Description'},
-        {id:2,title:'JavaScript 2',body:'Description'},
-        {id:3,title:'JavaScript 3',body:'Description'},
-    ])
-    const [posts2,setPosts2] = useState([
-        {id:1,title:'Python',body:'Description'},
-        {id:2,title:'Python 2',body:'Description'},
-        {id:3,title:'Python 3',body:'Description'},
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'},
     ])
 
     return (
         <div className="App">
-        <PostList posts={posts} title='Список постов 1'/>
-            <PostList posts={posts2} title='Список постов 2'/>
+            <form>
+                <input type="text" placeholder='Название поста'/>
+                <input type="text" placeholder='Описание поста'/>
+                <MyButton>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title='Посты про JS'/>
+
         </div>
     );
 }
